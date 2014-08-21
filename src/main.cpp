ed547b36bd94ff28429c372f885494a3efe3725b
Behnam Asadi <behnam@informatik.uni-bremen.de>
2014-08-21 23:00:59 +0200

    transformation updated

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Eigen/Dense>
 #include <Eigen/Eigenvalues>
 #include <Eigen/Geometry>
+#include <kdl/frames.hpp>
 using namespace Eigen;
 using namespace std;
 
@@ -74,7 +75,33 @@ void DynamicSizeMatrix()
 			A(i,j)=i+j;
 		}
 	}
-	cout<<"A is : " <<A<<endl;
+    cout<<"A is : " <<A <<endl;
+
+    Eigen::Matrix2d a_2d;
+    a_2d.setRandom();
+    std::cout<<"a_2d: " <<a_2d <<std::endl;
+//    setOnes()
+//    setConstant()
+//    setRandom()
+//    setLinSpaced()
+
+
+    std::cout<<"a_2d.minCoeff(): " <<a_2d.minCoeff() <<std::endl;
+    std::cout<<"a_2d.maxCoeff(): " <<a_2d.maxCoeff() <<std::endl;
+
+//    m.minCoeff()
+//    m.maxCoeff()
+//    m.prod()
+//    m.sum()
+//    m.mean()
+//    m.trace()
+//    m.colwise()
+//    m.rowwise()
+//    m.all()
+//    m.any()
+//    m.lpNorm<p>()
+//    m.lpNorm<Infinity>()
+
 }
 
 void EigenValueSolver()
@@ -111,122 +138,245 @@ void EigenValueSolver()
 	//       << ces.eigenvectors() * ces.eigenvalues().asDiagonal() * ces.eigenvectors().inverse() << endl;
 }
 
-//	Affine Transformation
-//	An affine transformation is equivalent to a linear transformation followed by a translation.
-//	linear transformation:
-//		rotation
-//		reflection
-//		scaling
-//		horizontal shear mapping
-//		squeeze mapping
-//		projection
 
-
-//If you are working with OpenGL 4x4 matrices then Affine3f and Affine3d are what you want.
-//Since Eigen defaults to column-major storage, you can directly use the Transform::data() method to pass your transformation matrix to OpenGL.
-
-//construct a Transform:
-//	Transform t(AngleAxis(angle,axis));
-//or like this:
-//	Transform t;
-//	t = AngleAxis(angle,axis);
-
-
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//But note that unfortunately, because of how C++ works, you can not do this:
-//  Transform t = AngleAxis(angle,axis);
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-void AffineTransformation()
+void Transformation()
 {
-//	2D rotation from an angle
-	float angle_in_radian=M_PI;
-	Rotation2D<float> rot2( angle_in_radian);
+    //If you are working with OpenGL 4x4 matrices then Affine3f and Affine3d are what you want.
+    //Since Eigen defaults to column-major storage, you can directly use the Transform::data() method to pass your transformation matrix to OpenGL.
 
-//	3D rotation as an angle + axis(The axis vector must be normalized!)
-	float ax,ay,az;
-	AngleAxis<float> aa(angle_in_radian, Vector3f(ax,ay,az));
+    //construct a Transform:
+    //	Transform t(AngleAxis(angle,axis));
+    //or like this:
+    //	Transform t;
+    //	t = AngleAxis(angle,axis);
 
 
-//	N-D Scaling
-	float sx,sy,sz,s;
-	int N=10;
-	VectorXf vecN(N);
-	Scaling(sx, sy);
-	Scaling(sx, sy, sz);
-	Scaling(s);
-	Scaling(vecN);
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    //But note that unfortunately, because of how C++ works, you can not do this:
+    //  Transform t = AngleAxis(angle,axis);
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-//	N-D Translation
-	float tx, ty,tz;
-	Translation<float,2>(tx, ty);
-	Translation<float,3>(tx, ty, tz);
-//	Translation<float,N>(s);
-//	Translation<float,N>(vecN);
+//
 
-//	N-D Affine transformation
-//	Transform<float,N,Affine> t = concatenation_of_any_transformations;
-//	Translation t;
-//	t= Translation<float,3>(tx, ty, tz);
-	Translation<float,3> t(tx, ty, tz);
-	Transform<float,3,Affine> AffineTranslation = t * aa * Scaling(s);
+/*
 
+yaw:
+    A yaw is a counterclockwise rotation of alpha about the  z-axis. The rotation matrix is given by
+
+    R_z
+
+    |cos(alpha) -sin(alpha) 0|
+    |sin(apha)   cos(alpha) 0|
+    |    0            0     1|
+
+pitch:
+    R_y
+    A pitch is a counterclockwise rotation of  beta about the  y-axis. The rotation matrix is given by
+
+    |cos(beta)  0   sin(beta)|
+    |0          1       0    |
+    |-sin(beta) 0   cos(beta)|
+
+roll:
+    A roll is a counterclockwise rotation of  gamma about the  x-axis. The rotation matrix is given by
+    R_x
+    |1          0           0|
+    |0 cos(gamma) -sin(gamma)|
+    |0 sin(gamma)  cos(gamma)|
+
+
+
+    It is important to note that   R_z R_y R_x performs the roll first, then the pitch, and finally the yaw
+    Roration matrix: R_z*R_y*R_x
+
+
+
+
+
+*/
+
+
+
+
+    double roll, pitch, yaw;
+    roll=M_PI/3;
+    pitch=M_PI/4;
+    yaw=M_PI/6;
+    Eigen::AngleAxisd rollAngle(roll, Eigen::Vector3d::UnitX());
+    Eigen::AngleAxisd pitchAngle(pitch, Eigen::Vector3d::UnitY());
+    Eigen::AngleAxisd yawAngle(yaw, Eigen::Vector3d::UnitZ());
+
+
+
+
+    Eigen::Quaternion<double> q =  yawAngle*pitchAngle *rollAngle;
+
+    Eigen::Matrix3d rotationMatrix = q.matrix();
+    std::cout<<rotationMatrix <<std::endl;
+
+
+
+    KDL::Frame F;
+    F.M=F.M.RPY(roll, pitch, yaw);
+    std::cout<<F.M(0,0) <<" " <<F.M(0,1)<<" " <<F.M(0,2) <<std::endl;
+    std::cout<<F.M(1,0) <<" " <<F.M(1,1)<<" " <<F.M(1,2) <<std::endl;
+    std::cout<<F.M(2,0) <<" " <<F.M(2,1)<<" " <<F.M(2,2) <<std::endl;
+
+
+
+
+
+    Eigen::Affine3d transform_2 = Eigen::Affine3d::Identity();
+
+    // Define a translation of 2.5 meters on the x axis.
+    transform_2.translation() << 2.5, 0.0, 0.0;
+
+    // The same rotation matrix as before; tetha radians arround Z axis
+    transform_2.rotate (yawAngle*pitchAngle *rollAngle );
+    std::cout<<transform_2.matrix() <<std::endl;
+}
+
+void DeterminingRollPitchYaw()
+{
+/*  http://planning.cs.uiuc.edu/node103.html
+
+    |r11 r12 r13 |
+    |r21 r22 r23 |
+    |r31 r32 r33 |
+
+    yaw: alpha=arctan(r21/r11)
+    pitch: beta=arctan(-r31/sqrt( r32^2+r33^2 ) )
+    roll: gamma=arctan(r32/r33)
+*/
+    double roll, pitch, yaw;
+    roll=M_PI/3;
+    pitch=M_PI/4;
+    yaw=M_PI/6;
+    Eigen::AngleAxisd rollAngle(roll, Eigen::Vector3d::UnitX());
+    Eigen::AngleAxisd pitchAngle(pitch, Eigen::Vector3d::UnitY());
+    Eigen::AngleAxisd yawAngle(yaw, Eigen::Vector3d::UnitZ());
+
+    Eigen::Quaternion<double> q =  yawAngle*pitchAngle *rollAngle;
+    Eigen::Matrix3d rotationMatrix = q.matrix();
+
+
+
+    std::cout<<"yaw is Pi/" <<M_PI/atan2( rotationMatrix(1,0),rotationMatrix(0,0) ) <<std::endl;
+//    std::cout<<"pitch: " << <<std::endl;
+    std::cout<<"roll is Pi/" <<M_PI/atan2( rotationMatrix(2,1),rotationMatrix(2,2) ) <<std::endl;
 
 
 
 }
 
-void  Test()
+void define_arbitrary_matrix()
 {
-//	When setting up an AngleAxis object, the axis vector must be normalized.
-	Matrix3f m;
-	m = AngleAxisf(0.25*M_PI, Vector3f::UnitX())  * AngleAxisf(0.5*M_PI,  Vector3f::UnitY())  * AngleAxisf(0.33*M_PI, Vector3f::UnitZ());
-//	cout << m << endl << "is unitary: " << m.isUnitary() << endl;
-//	cout<<"Vector3f::UnitX() " <<Vector3f::UnitX() <<endl;
-//	cout<<"Vector3f::UnitY() " <<Vector3f::UnitY() <<endl;
-//	cout<<"Vector3f::UnitZ() " <<Vector3f::UnitZ() <<endl;
+//    template<typename _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+    Eigen::Matrix< double ,2,3> my_matrix;
+    my_matrix<<1,2,3,4,5,6;
+    std::cout<< my_matrix<<std::endl;
+}
 
-////	If you are working with OpenGL 4x4 matrices then Affine3f and Affine3d are what you want.
-////	typedef Transform< float,3, Affine > 	Affine3f
-//	Affine3f m1;
-//	cout<< m1.Rows<<endl;
-//	cout<< m1.Dim <<endl;
-//	cout<< m1.Mode <<endl;
-////	m1 = AngleAxis3f(..);
-////
-////	Affine3f m2;
-////	m2 = Scaling(..);
-
-	Vector3f offset;
-	Matrix4f  	transform;
-	transform(0,0)=m(0,0);
-	transform(0,1)=m(0,1);
-	transform(0,2)=m(0,2);
-	transform(1,0)=m(1,0);
-	transform(1,1)=m(1,1);
-	transform(1,2)=m(1,2);
-	transform(2,0)=m(2,0);
-	transform(2,1)=m(2,1);
-	transform(2,2)=m(2,2);
-
-	transform(0,3)=offset(0);
-	transform(1,3)=offset(1);
-	transform(2,3)=offset(2);
-
-	transform(3,0)=0.0;
-	transform(3,1)=0.0;
-	transform(3,2)=0.0;
-	transform(3,3)=1.0;
+void ArrayExample()
+{
+/*The Array class provides general-purpose arrays, as opposed to the Matrix class which is intended for linear algebra.
+ * Furthermore, the Array class provides an easy way to perform coefficient-wise operations, which might not have a linear algebraic meaning, such as adding a constant to every coefficient in the array or multiplying two arrays coefficient-wise.*/
+    //ArrayXf
+    Array<float,Dynamic,1> a1;
+    //Array3f
+    Array<float,3,1> a2;
+    //ArrayXXd
+    Array<double,Dynamic,Dynamic> a3;
+    //Array33d
+    Array<double,3,3> a4;
+    Eigen::Matrix3d matrix_from_array = a4.matrix();
 
 
 }
 
+void CoefficientWiseOperation()
+{
+    /*Must convert Matrix<T> to Array<T> using array() member     function in order to perform coefficient-wise operations*/
+    Eigen::Matrix2i m;
+    m<<-1,2,3,4;
+
+    Eigen::Matrix2i a;
+    a<<2,2,3,4;
+    std::cout<<"m: " << m<<std::endl;
+    std::cout<<"m.array() -2 " << m.array() -2<<std::endl;
+    std::cout<<"m.array().abs() " <<m.array().abs()<<std::endl;
+    std::cout<<"m.array().square() " <<m.array().square()<<std::endl;
+    std::cout<<"m.array()*a.array() " <<m.array()*a.array()<<std::endl;
+//    pow()
+//    exp()
+//    log()
+//    sqrt()
+}
+
+void EigenMapExample()
+{
+    int array[9];
+    for(int i = 0; i < 9; ++i)
+    {
+         array[i] = i;
+    }
+
+//    Eigen::MatrixXi a(3,3);
+    Eigen::MatrixXi a(9,1);
+    a= Map<Matrix3i>(array);
+    cout << a << endl;
+
+}
+
+void LinearSolving()
+{
+    //Ax=b, x=?
+    MatrixXf A(3,3);
+    VectorXf b(3,1);
+    A << 1,2,3,  4,5,6,  7,8,10;
+    b << 3, 3, 4;
+    cout << "Here is the matrix A:\n" << A << endl;
+    cout << "Here is the vector b:\n" << b << endl;
+    VectorXf x = A.colPivHouseholderQr().solve(b);
+    cout << "The solution using colPivHouseholderQr is:\n" << x << endl;
+
+
+
+    cout << "The solution using partialPivLu() is:\n" << A.partialPivLu().solve(b) << endl;
+
+/*
+    partialPivLu()
+    fullPivLu()
+    householderQr()
+    colPivHouseholderQr()
+    fullPivHouseholderQr()
+    llt()
+    ldlt()
+*/
+
+}
+
+void InverseOfMatrix()
+{
+    Matrix3f A;
+    A << 1, 2, 1,
+         2, 1, 0,
+         -1, 1, 2;
+    cout << "Here is the matrix A:\n" << A << endl;
+    cout << "The determinant of A is " << A.determinant() << endl;
+    cout << "The inverse of A is:\n" << A.inverse() << endl;
+
+}
 
 int main()
 {
-//	FixedSizedMatrixMatrix();
-//	DynamicSizeMatrix();
-	Test();
-
-
+//    FixedSizedMatrixMatrix();
+//    DynamicSizeMatrix();
+//    EigenMapExample();
+//    CoefficientWiseOperation();
+//    define_arbitrary_matrix();
+//    LinearSolving();
+//    InverseOfMatrix();
+//    Transformation();
+    DeterminingRollPitchYaw();
 }
